4cbc00dff5ba865c44eb28464ecd4bf9a0105ae9
Abhishek Chakraborty <chakraborty@silexica.com>
2019-04-02 08:55:08 +0200

    Redux tutorial from https://www.valentinog.com/blog/redux/

---
+++ redux-2019/src/js/reducers/index.js
@@ -0,0 +1,21 @@
+import { ADD_ARTICLE } from "../constants/action-types";
+
+const initialState = {
+  articles: []
+};
+function rootReducer(state = initialState, action) {
+  // When the action type matches a valid clause the reducer calculates the next state and returns a new object.
+  if (action.type === ADD_ARTICLE) {
+    // state.articles.push(action.payload);
+    // First we can return a new state, ie a new JavaScript object with Object.assign.
+    // This way we keep the original state immutable. Then we can use Array.prototype.
+    // concat in place of Array.prototype.push for keeping the initial array immutable:
+    return Object.assign({}, state, {
+      articles: state.articles.concat(action.payload)
+    });
+  }
+
+  //   it should return at least the initial state when no action type matches.
+  return state;
+}
+export default rootReducer;
